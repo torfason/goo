9a412b3a73b74e30a76184ee72e5b7673dc2a724
Magnus Thor Torfason <m@zulutime.net>
2021-08-17 22:38:23 +0000

    Add a message to a test, create a new version

--- goo_test.go
+++ goo_test.go
@@ -10,10 +10,10 @@ import (
 func TestMain(t *testing.T) {
 
 	t.Run("assert.EqualInt", func(t *testing.T) {
-		assert.EqualInt(t, 1, 1)
+		assert.EqualInt(t, 1, 1, "A message is optional.")
 	})
 
 	t.Run("assert.Error", func(t *testing.T) {
-		assert.Error(t, errors.New("an error"), "A message is optional")
+		assert.Error(t, errors.New("an error"), "A message is optional.")
 	})
 }
